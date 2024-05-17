5ee5462f4edd048d11b1867dd769abfbc3595fe4
n-anurag <anuragneupane58@gmail.com>
2024-05-17 17:07:26 +0545

    qr code created

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
-import { useState } from 'react'
-
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
- 
-
-  return (
-    <>
-    <h1>QR code Generator</h1>
-    
-    </>
-  )
+  const [url, setUrl] = useState('');
+const [show,setShow]=useState(false)  
+const handleClick=(e)=>{
+  setShow(true);
 }
 
-export default App
+  return (
+    <div className="App"> 
+      <h1>QR Code Generator</h1>
+      <div>
+        <input
+          type="url"
+          name="qr-code"
+          placeholder="Enter URL"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+        <button onClick={handleClick}>Generate</button>
+      </div>
+      {show && url && (
+        <div className="qr-code">
+         <img
+            src={`https://qrcode.tec-it.com/API/QRCode?data=${url}`}
+            alt="QR Code"
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
